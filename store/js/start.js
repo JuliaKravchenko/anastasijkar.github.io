0376aedc5ed2eea69b79ee0b8c0d1e60d5d384dc
anastasijkar <rudich1anastasia@gmail.com>
2016-12-18 16:43:20 +0200

    'store'

---
+++ store/js/start.js
@@ -0,0 +1,27 @@
+var currentItem;
+
+function next() {
+	var elems = document.getElementsByClassName('carousel__item');
+	elems[currentItem].style.opacity = 0;
+	elems[currentItem].style.zIndex = -100;
+	currentItem++;
+	if (currentItem > 2) currentItem = 0;
+	elems[currentItem].style.opacity = 1;
+	elems[currentItem].style.zIndex = 100;
+}
+
+function prev() {
+	var elems = document.getElementsByClassName('carousel__item');
+	elems[currentItem].style.opacity = 0;
+	elems[currentItem].style.zIndex = -100;
+	currentItem--;
+	if (currentItem < 0) currentItem = 2;
+	elems[currentItem].style.opacity = 1;
+	elems[currentItem].style.zIndex = 100;
+}
+
+window.onload = function () {
+	refreshBag();
+	currentItem = 0;
+	setInterval(next, 6000);
+}
